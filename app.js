dfaa5b64e116c84591c8112cb392b375e0c792cf
karoliks <karoline.k.sabo@gmail.com>
2020-09-23 09:57:46 +0200

    Change title

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ function smoothScroll(target, duration) {
   var target = document.querySelector(target);
   var targetPosition = target.getBoundingClientRect().top;
   var startPosition = window.pageYOffset;
-  var distance = targetPosition - startPosition;
   var startTime = null;
-
-  console.log(targetPosition);
-  console.log(startPosition);
-  console.log(distance);
 
   function animation(currentTime) {
     if (startTime == null) startTime = currentTime;
     var timeElapsed = currentTime - startTime;
-    var run = ease(timeElapsed, startPosition, targetPosition, duration);
+    var run = ease(timeElapsed, startPosition, targetPosition + 5, duration);
     window.scrollTo(0, run);
     if (timeElapsed < duration) requestAnimationFrame(animation);
   }
@@ -37,31 +32,61 @@ var lineLink = document.querySelector(".lineLink");
 introLink.addEventListener("click", function() {
   smoothScroll(".introSection", 1000);
   // Make the last clickes link unactive, and this active
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  // var current = document.getElementsByClassName("active");
+  // current[0].className = current[0].className.replace(" active", "");
+  // this.className += " active";
 });
 
 svgLink.addEventListener("click", function() {
   smoothScroll(".svgSection", 1000);
   // Make the last clickes link unactive, and this active
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  // var current = document.getElementsByClassName("active");
+  // current[0].className = current[0].className.replace(" active", "");
+  // this.className += " active";
 });
 
 paintLink.addEventListener("click", function() {
   smoothScroll(".paintSection", 1000);
   // Make the last clickes link unactive, and this active
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  // var current = document.getElementsByClassName("active");
+  // current[0].className = current[0].className.replace(" active", "");
+  // this.className += " active";
 });
 
 lineLink.addEventListener("click", function() {
   smoothScroll(".lineSection", 1000);
   // Make the last clickes link unactive, and this active
+  // var current = document.getElementsByClassName("active");
+  // current[0].className = current[0].className.replace(" active", "");
+  // this.className += " active";
+});
+
+function scrollUpdate() {
+  var svgSection = document.querySelector(".svgSection");
+  var svgPosition = svgSection.getBoundingClientRect().top;
+
+  var lineSection = document.querySelector(".lineSection");
+  var linePosition = lineSection.getBoundingClientRect().top;
+
+  var paintSection = document.querySelector(".paintSection");
+  var paintPosition = paintSection.getBoundingClientRect().top;
+
+  console.log(svgPosition);
+  if (paintPosition <= 0) {
+    changeToThis(paintLink);
+  } else if (linePosition <= 0) {
+    changeToThis(lineLink);
+  } else if (svgPosition <= 0) {
+    changeToThis(svgLink);
+  } else {
+    changeToThis(introLink);
+  }
+}
+
+function changeToThis(link) {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-});
+  link.className += " active";
+}
+
+window.addEventListener("scroll", scrollUpdate);
